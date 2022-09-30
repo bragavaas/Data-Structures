efd9c9e3d862cb317c0946dc8a15ada020f2f8e6
bragavaas <bragavaas@gmail.com>
2022-09-30 09:39:32 -0300

    Pilha: Vazia, Cheia, Insere, Remove topo

--- Pilha.cpp
+++ Pilha.cpp
@@ -4,3 +4,43 @@ Pilha::Pilha()
 {
 	topo = -1;
 }
+
+int Pilha::insere_topo(int n)
+{
+	if( is_cheia() )
+	{
+		return 0;
+	}
+	++topo;
+	num[topo] = n;
+	return n;
+}
+
+int Pilha::remove_topo()
+{
+	int temp;
+	if (is_vazia())
+	{
+		return 0;
+	}
+	temp = num[topo];
+	--topo;
+	return temp;
+
+}
+
+int Pilha::is_vazia()
+{
+	return (topo == 1 ) ? 1 : 0;
+}
+
+int Pilha::is_cheia()
+{
+	return (topo == (tam-1)) ? 1 : 0;
+}
+
+void Pilha::imprimir()
+{
+}
+
+int 
