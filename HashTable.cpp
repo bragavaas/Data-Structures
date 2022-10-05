65ab3080bce69d46395ec80266d7c5164d817ea3
bragavaas <bragavaas@gmail.com>
2022-10-04 23:08:49 -0300

    HashTable CPP coding jesus

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,1 +1,84 @@
 #include "HashTable.h"
+
+bool HashTable::is_vazia() const
+{
+	int soma{};
+	for (int i{}; i < hashGroups; i++)
+	{
+		soma += Tabela[i].size();
+	}
+	if (!soma)
+	{
+		return true;
+	}
+	return false;
+}
+
+int HashTable::hash_Function(int key)
+{
+	return key % hashGroups;
+}
+
+void HashTable::inserir_Item(int key, string value)
+{
+	int hashValue = hash_Function(key);
+	auto& celula = Tabela[hashValue];
+	auto bItr = begin(celula);
+	bool key_existe = false;
+	for (; bItr != end(celula); bItr++)
+	{
+		if (bItr->first == key)
+		{
+			key_existe = true;
+			bItr->second = value;
+			bItr = celula.erase(bItr);
+			cout << "[ALERTA] Chave existe. Valor Atualizado." << endl;
+			break;
+		}
+
+	}
+	if (!key_existe) celula.emplace_back(key, value);
+	
+}
+
+void HashTable::remover_Item(int key)
+{
+	int hashValue = hash_Function(key);
+	auto& celula = Tabela[hashValue];
+	auto bItr = begin(celula);
+	bool key_existe = false;
+
+	for (; bItr != end(celula); bItr++)
+	{
+		if (bItr->first == key)
+		{
+			key_existe = true;
+			bItr = celula.erase(bItr);
+			cout << "[INFO] Par Removido" << endl;
+			break;
+		}
+
+	}
+	if (!key_existe)
+	{
+		cout << "[ALERTA] Chave nao encontrada. Par nao removido." << endl;
+	}
+	return;
+}
+
+void HashTable::imprime_Tabela()
+{
+	for (int i{}; i < hashGroups; i++)
+	{
+		if (Tabela[i].size() == 0) continue;
+		auto bItr = Tabela[i].begin();
+		for (; bItr != Tabela[i].end(); bItr++)
+		{
+			cout << "CHAVE: " << bItr->first << " Value: " << bItr->second << endl;
+		}
+	}
+	return;
+
+
+}
+
--- main.cpp
+++ main.cpp
@@ -2,13 +2,39 @@
 #include "ListaEncadeada.h"
 #include "Pilha.h"
 #include "Heap.h"
+#include "HashTable.h"
+
+using namespace std;
+
 int main()
 {
-	MaxHeap h;
-	h.inserirItem(10);
-	h.inserirItem(70);
-	h.inserirItem(2);
-	h.inserirItem(200);
-	
-	
+	HashTable h;
+
+	if (h.is_vazia())
+	{
+		cout << "HT Vazia" << endl;
+	}
+	else
+	{
+		cout << "HT nao vazia" << endl;
+	}
+
+	h.inserir_Item(905, "Jim");
+	h.inserir_Item(925, "Pam");
+	h.inserir_Item(427, "Dwight");
+	h.inserir_Item(969, "Charlie");
+	h.inserir_Item(421, "Half");
+	h.inserir_Item(754, "Denise");
+	h.inserir_Item(422, "Piazzi");
+
+	h.imprime_Tabela();
+
+	if (h.is_vazia())
+	{
+		cout << "\nHT Vazia" << endl;
+	}
+	else
+	{
+		cout << "\nHT nao vazia" << endl;
+	}
 }
